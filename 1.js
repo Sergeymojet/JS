b7ce02d66743d913c222d32a317d33da6ed0a38b
Sergeymojet <sergeymojet@yandex.ru>
2023-01-23 22:39:42 +0300

    HW_JS2.2

--- 1.js
+++ 1.js
@@ -1,3 +1,3 @@
 Math.round
-let inputdeg = +parseInt(prompt('Введите температуру в градусах Цельсия'));
+let inputdeg = +prompt("Введите температуру в градусах Цельсия");
 alert(`Цельсия: ${inputdeg}\  Фаренгейт: ${Math.round((9 / 5) * inputdeg + 32)}`)
